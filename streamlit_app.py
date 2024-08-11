87dfdac09c86151acd5fb8fc983f9fab626b836b
world-source <jcmye1@schools.vic.edu.au>
2024-08-12 09:40:20 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 from datetime import date
 import yfinance as yf 
-from prophet import Prophet
+import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
