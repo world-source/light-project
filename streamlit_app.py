84857ba493cc3e0741f90e4d5a105b0c300b2eb9
world-source <jcmye1@schools.vic.edu.au>
2024-08-15 11:56:42 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.set_page_config(
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock Prediction App")
+st.markdown("<h1 style='text-align: center; color: red;'>Stock Prediction App</h1>", unsafe_allow_html=True)
 
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'NVDA', 'INTC', 'AMZN', 'EBAY', 'AAL', 'AMD', 'NFLX', 'PEP', 'ADBE', 'META', 'TXN', 'ABNB', 'PYPL', 'LYFT')
