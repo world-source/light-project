4a92a5fd46d1a6e4c29d92250824fd092a9f3a46
world-source <jcmye1@schools.vic.edu.au>
2024-08-15 12:12:47 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,10 +80,11 @@ forecast = m.predict(future)
 # Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast.tail())
-st.write(forecast.tail())
+
     
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 st.plotly_chart(fig1)
 
 st.write("Forecast components")
