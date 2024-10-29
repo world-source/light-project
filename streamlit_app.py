041bade77213e14c2a8a78743e44b8a0566be158
world-source <jcmye1@schools.vic.edu.au>
2024-10-29 23:05:00 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 import warnings
 warnings.simplefilter("ignore", category=FutureWarning)
+import pandas as pd
 
 import streamlit.web.bootstrap
 
